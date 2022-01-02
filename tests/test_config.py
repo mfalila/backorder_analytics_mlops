74f91b74bc6553b8937497aa0dbc22596025b431
sl202194@hotmail.com <sl202194@hotmail.com>
2022-01-02 13:32:28 -0500

    ci-cd pipeline added

--- tests/test_config.py
+++ tests/test_config.py
@@ -62,7 +62,7 @@ def test_form_response_correct_range(sample_dict=input_data["correct_range"]):
 
 def test_api_response_correct_range(sample_dict=input_data["correct_range"]):
     res = api_response(sample_dict)
-    res = float((([x for x in res.values()])[0]).strip('"'))
+    #res = float((([x for x in res.values()])[0]).strip('"'))
     assert TARGET_range["min"] <= res <= TARGET_range["max"]
 
 
